d2d5ee35cf32a8e677dda3866d8f64249c8aa322
andreagy <andi.gyimoti@gmail.com>
2025-02-13 22:43:32 +0200

    implement temperature reading and create json file

--- TemperatureSensor.h
+++ TemperatureSensor.h
@@ -5,12 +5,14 @@
 #ifndef DATASTRUCTUREALGORITHMSPROJECT_TEMPERATURESENSOR_H
 #define DATASTRUCTUREALGORITHMSPROJECT_TEMPERATURESENSOR_H
 
+#define SENSORS_PATH "/sys/bus/w1/devices/"
 
 #include <iostream>
 #include <thread>
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <fstream>
 #include "InfoNode.h"
 #include "QueueManager.h"
 
@@ -18,24 +20,26 @@ using namespace std;
 class TemperatureSensor {
 public:
     TemperatureSensor(const string name, const string sensorDir, QueueManager& dequeue, int interval=1): name(name), sensorDir(sensorDir), queue_manager(dequeue), interval(interval){
-        //TODO: the best is to open the file only once here when constructing class
-        //readFile();
+        //open the file only once here when constructing object
+        openFile();
     };
     ~TemperatureSensor();
+
     void start_temp_reading_thread();
     void stop_temp_reading_thread();
-    int get_temperature();
+    float get_temperature();
+
 private:
     const string name;
     const string sensorDir;
     float temp=0;
     int interval;
     bool terminated = false;
+    ifstream sensor_file;
     thread temperature_reader;
     QueueManager & queue_manager;
     void read_temperature();
-    int read_sensor();
-    void readFile();
+    void openFile();
 };
 
 
--- main.cpp
+++ main.cpp
@@ -6,18 +6,55 @@
 using namespace std;
 using json = nlohmann::json;
 int main(){
-    //test json
-    json j = {{"sensor", "test_sensor"}, {"temperature", 30}};
-    std::cout << j.dump(4) << std::endl;
+    // Open a file to store temperature data
+    ofstream output_file("temperature_data.json");
+    if (!output_file.is_open()) {
+        cerr << "Error: Unable to open file for writing." << endl;
+        return EXIT_FAILURE;
+    }
+    // JSON array to store multiple readings
+    json json_array = json::array();
+
+    // Map sensor IDs to their corresponding directory names
+    map<int, string> sensorMap {
+            {1, "28-00000087fb7c"},
+            {2, "28-00000085e6ff"},
+            {3, "28-000000849be2"}
+    };
+    TemperatureSensor temp_sensor1("sensor1", sensorMap[1],  queue_manager, 3);
+    TemperatureSensor temp_sensor2("sensor2", sensorMap[2], queue_manager, 3);
+    TemperatureSensor temp_sensor3("sensor2", sensorMap[3], queue_manager, 3);
 
     QueueManager queue_manager;
-    TemperatureSensor temp_sensor1("sensor1", 0,  queue_manager, 3);
-    TemperatureSensor temp_sensor2("sensor2",1, queue_manager);
+
     temp_sensor1.start_temp_reading_thread();
     temp_sensor2.start_temp_reading_thread();
+    temp_sensor3.start_temp_reading_thread();
     this_thread::sleep_for(chrono::seconds(20));
 
     temp_sensor1.stop_temp_reading_thread();
     temp_sensor2.stop_temp_reading_thread();
+    temp_sensor3.stop_temp_reading_thread();
+
+    // Process stored temperature readings from the queue
+    while (!queue_manager.is_empty()) {
+        InfoNode node = queue_manager.pop_front();
+
+        // Convert InfoNode data to JSON
+        json sensor_json = {
+                {"sensor", node.name},
+                {"timestamp", node.timestamp},
+                {"temperature", node.temperature}
+        };
+
+        // Print JSON to console
+        cout << sensor_json.dump(4) << endl;
+        // Add JSON object to array
+        json_array.push_back(sensor_json);
+    }
+    // Write the JSON array to the file
+    output_file << json_array.dump(4) << endl;
+    output_file.close();
+
     return EXIT_SUCCESS;
 }
